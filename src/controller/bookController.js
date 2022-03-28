171715c89e58763bfd47225159ff702ab762a5cb
Rohan_Pagare <rohanpagare7599@gmail.com>
2022-03-28 14:43:56 +0530

    add getBook function

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -1,1 +1,28 @@
 const bookModel = require("../model/bookModel")
+
+const getBook = async (req, res) => {
+    // const filterKeys = ["userId", "category", "subcategory"]
+    try{
+        const data = req.query
+        const filter = {
+            isDeleted: false,
+            ...data
+        }
+        const books = await bookModel.find(filter).select({"title": 1,"excerpt": 1,"userId": 1,"category":1,"reviews": 1,"releasedAt": 1})
+        if(books.length === 0){
+            return res.status(404).send({status: true, message: "no book founds"})
+        }
+        // if(books.length == 1){
+        //     if(!filterKeys.includes(Object.keys(data)[0])){
+        //         return res.status(404).send({status: true, message: `can not filter using (${Object.keys(data)[0]}) query`})
+        //     }
+        // }
+        return res.status(200).send({status: true, message: "Books list", data: books})
+    }catch(e){
+        return res.status(500).send({status: false, message: e.message})
+    }
+}
+
+const deleteBook = (req)
+
+module.exports.getBook = getBook;
--- src/route/route.js
+++ src/route/route.js
@@ -1,13 +1,17 @@
 const express = require("express")
 
 const userContrller = require("../controller/userController")
+const bookController = require("../controller/bookController")
 
 const router = express.Router()
 
-
+// user
 router.post("/register", userContrller.register)
 
 router.post("/login", userContrller.login)
+
+// book
+router.get("/books", bookController.getBook)
 
 module.exports = router;
 
