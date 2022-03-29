6c9d0dde0b7a4e64c9c32c44d041a1b691e0d4b0
Rohan_Pagare <rohanpagare7599@gmail.com>
2022-03-29 12:37:04 +0530

    implement autherisation and authentication

--- src/route/route.js
+++ src/route/route.js
@@ -2,6 +2,7 @@ const express = require("express")
 
 const userController = require("../controller/userController")
 const bookController = require("../controller/bookController")
+const mw = require("../middleware/auth")
 
 const router = express.Router()
 
@@ -11,15 +12,17 @@ router.post("/register", userController.register)
 router.post("/login", userController.login)
 
 // book
-router.post('/books',bookController.createBook)
+router.post('/books',mw.authentication,bookController.createBook)
 
-router.get("/books", bookController.getBook)
+router.get("/books",mw.authentication, bookController.getBook)
 
-router.get("/bookss/:bookId",bookController.getBooks)
+router.get("/bookss/:bookId",mw.authentication, mw.authorisation, bookController.getBooks)
 
-router.put("/books/:bookId",bookController.updateBook)
+router.put("/books/:bookId",mw.authentication, mw.authorisation,bookController.updateBook)
 
-router.delete("/books/:bookId", bookController.deleteBook)
+router.delete("/books/:bookId",mw.authentication, mw.authorisation, bookController.deleteBook)
+
+// review
 
 
 router.get("*", (req, res) => {
